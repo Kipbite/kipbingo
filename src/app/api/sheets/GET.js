95a74a76ffb14d6547debf97b9192447796a42b4
chesterblack <chez.black@hotmail.co.uk>
2024-01-04 22:00:21 +0000

    Fixed missing response params

--- src/app/api/sheets/GET.js
+++ src/app/api/sheets/GET.js
@@ -15,15 +15,20 @@ export default async function sheetsEndpointGet(request) {
   const { searchParams } = new URL(request.url);
   const limit = parseInt( searchParams.get('limit') ) ?? 1;
 
-  const response = await db
-    .collection( 'sheets' )
-    .find()
-    .limit( limit )
-    .toArray()
+  let response;
+  try {
+    response = await db
+      .collection( 'sheets' )
+      .find()
+      .limit( limit )
+      .toArray()
 
-  if ( limit === 1 ) {
-    return NextResponse.json(response[0]);
+    if ( limit === 1 ) {
+      return NextResponse.json({ success: true, message: response[0] });
+    }
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error });
   }
 
-  return NextResponse.json(response);
+  return NextResponse.json({ success: true, message: response });
 }
