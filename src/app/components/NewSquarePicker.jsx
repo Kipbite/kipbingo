921924dadbd916f9f794cc3e8caf790d2f6e1c3b
chesterblack <chez.black@hotmail.co.uk>
2024-02-23 11:51:36 +0000

    Adding a new square possibility now can be done on pressing enter, and autofocuses on the new square input when submitted

--- src/app/components/NewSquarePicker.jsx
+++ src/app/components/NewSquarePicker.jsx
@@ -6,30 +6,49 @@ export default function NewSquarePicker({}) {
   const { updateSquares, setUpdateSquares, gameType } = useContext(AdminContext);
   const [ newSquare, setNewSquare ] = useState('');
 
+  async function submitNewSquare() {
+    await sendApiRequest(
+      'POST',
+      '/squares',
+      null,
+      {
+        game: gameType,
+        text: newSquare,
+      }
+    );
+    
+    setNewSquare('');
+    setUpdateSquares(updateSquares + 1);
+    document.querySelector('.new-possibility-input').focus();
+  }
+
   return (
     <div className="possibility new-possibility">
       <input
+        className="new-possibility-input"
         type="text"
         placeholder="New square"
         onChange={(e) => {
           setNewSquare(e.target.value);
         }}
+        onKeyUp={(e) => {
+          let isEnter = false;
+
+          if ( e.key !== undefined ) {
+            isEnter = e.key === "Enter";
+          } else if ( e.keyCode !== undefined ) {
+            isEnter = e.keyCode === 13;
+          }
+
+          if ( isEnter ) {
+            submitNewSquare()
+          }
+        }}
         value={newSquare}
       />
 
-      <button onClick={async () => {
-        await sendApiRequest(
-          'POST',
-          '/squares',
-          null,
-          {
-            game: gameType,
-            text: newSquare,
-          }
-        );
-        
-        setNewSquare('');
-        setUpdateSquares(updateSquares + 1);
+      <button onClick={() => {
+        submitNewSquare()
       }}>
         Add
       </button>
