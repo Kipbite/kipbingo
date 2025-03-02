9ace6804eccb42a9a14b84aa9e53eecb0d600585
chesterblack <chez.black@hotmail.co.uk>
2025-03-02 17:41:38 +0000

    Sets square active to true if you're trying to recreate an existing, inactive, square

--- src/app/api/squares/POST.js
+++ src/app/api/squares/POST.js
@@ -1,4 +1,5 @@
 import clientPromise from "@/app/lib/mongodb";
+import { ObjectId } from "bson";
 import { NextResponse } from "next/server";
 
 export default async function squaresEndpointPost(request) {
@@ -6,15 +7,34 @@ export default async function squaresEndpointPost(request) {
   const db = client.db(process.env.DATABASE);
   const body = await request.json();
   
+  let response = {};
   const insertDoc = {};
 
   insertDoc.game = body.game ? body.game.toString() : 'Unknown Game';
   insertDoc.text = body.text ? body.text.toString() : '';
-  insertDoc.active = true;
 
-  const response = await db
+  const getResponse = await db
     .collection( 'squares' )
-    .insertOne( insertDoc )
+    .find( insertDoc )
+    .toArray()
+  
+  if ( getResponse.length > 0 ) {
+    response = await db
+      .collection( 'squares' )
+      .updateOne(
+        { _id: new ObjectId( getResponse[0]._id ) },
+        { $set: {
+          active: true
+        } }
+      )
+  } else {
+    insertDoc.active = true;
+  
+    response = await db
+      .collection( 'squares' )
+      .insertOne( insertDoc )
+  }
+
 
   return NextResponse.json( response );
 } 
