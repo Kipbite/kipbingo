530bf5d11ee344a847510aeae3dc13be48a32acf
Chester Black <chez.black@hotmail.co.uk>
2026-03-06 18:15:17 +0000

    Renamed DiscordLogo

--- src/app/components/SignIn.jsx
+++ src/app/components/SignIn.jsx
@@ -1,13 +1,13 @@
 "use client"
 
-import DiscordIcon from "@/app/icons/discord-logo"
+import DiscordLogo from "@/app/icons/DiscordLogo"
 import { signIn } from "next-auth/react"
  
 export default function SignIn() {
   return (
     <>
       <button onClick={() => signIn("discord")} className="sign-in discord-sign-in">
-        <DiscordIcon />
+        <DiscordLogo />
         Sign in with Discord
       </button>
     </>
